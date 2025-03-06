40b385712131de77a12c697cb67f93a05ec2c05f
yalfmon0307 <montesyalfred0@gmail.com>
2025-03-06 14:33:26 -0600

    agregando estilos

--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -5,8 +5,8 @@ export function Home() {
     <>
       <h1>Bienvenido a la aplicacion TodoApp</h1>
       <p>Puedes iniciar sesion o registrarte</p>
-      <Link to="/login">Iniciar Sesión</Link>
-      <Link to="/register">Registrarme</Link>
+      <Link to="/login" className="link">Iniciar Sesión</Link>
+      <Link to="/register" className="link">Registrarme</Link>
     </>
   ) 
 }
