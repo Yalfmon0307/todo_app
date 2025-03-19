5fa58bfed3e4086efa622211e0bd9c605f71e5db
yalfmon0307 <montesyalfred0@gmail.com>
2025-03-19 11:46:32 -0600

    crando los script

--- src/db.js
+++ src/db.js
@@ -16,4 +16,12 @@ if (query) {
     console.log('Connected to the database');
 }
 
+const query2 = await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,username CHARACTER VARYING(50) NOT NULL,password CHARACTER VARYING(255) NOT NULL,email CHARACTER VARYING(100) NOT NULL,UNIQUE (email),UNIQUE (username));');
+
+const query1 = await pool.query('CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY,user_id INTEGER NOT NULL,task TEXT NOT NULL,completed BOOLEAN DEFAULT FALSE,created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (user_id) REFERENCES users(id));');
+
+if (query1 && query2) {
+    console.log('Tables created successfully');
+}
+
 export default pool;
